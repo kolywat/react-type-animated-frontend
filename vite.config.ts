1e59448cd22695b5bcfbc9295b24179cc38b281e
kolywat <maxnem93@gmail.com>
2025-07-12 02:30:45 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   server: {
     allowedHosts: [
       'localhost',
-      '282e-2001-2020-4325-e2ab-ace2-1e74-513f-396b.ngrok-free.app', // вставь свой домен от ngrok
     ],
   },
 })
